cdbef3e3cb723d3efbb598ef95ed7e090a9a059f
Louis <louisapstg@gmail.com>
2022-11-29 11:10:09 +0700

    feat/landing: add layout component

---
+++ src/components/LayoutLanding.jsx
@@ -0,0 +1,23 @@
+import Card from "./Card";
+import Testimonial from "./Testimonial";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import AboutUs from "./AboutUs";
+import Navbar from "./Navbar";
+import ClassesList from "./ClassesList";
+import InstructorInfo from "./InstructorInfo";
+import ClassesInfo from "./ClassesInfo";
+import ToTop from "./ToTop";
+
+export {
+	Card,
+	Testimonial,
+	Footer,
+	Hero,
+	AboutUs,
+	Navbar,
+	ClassesList,
+	InstructorInfo,
+	ClassesInfo,
+	ToTop,
+};
