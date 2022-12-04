ed0b593ce929547f98ffb27447da916d079447d6
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-12-04 14:04:28 +0700

    feat/dashboard: add mocks navbar collections and update link with mapping in navbar component

---
+++ src/mocks/navbarCollections.jsx
@@ -0,0 +1,22 @@
+export const navbarCollections = [
+	{
+		name: "Home",
+		path: "/#hero",
+	},
+	{
+		name: "About Us",
+		path: "/#about",
+	},
+	{
+		name: "Classes Info",
+		path: "/#classes-info",
+	},
+	{
+		name: "Class",
+		path: "/#classes-list",
+	},
+	{
+		name: "Testimoni",
+		path: "/#testimonial",
+	},
+];
