80678b20393a66ed5ed70d5fb4437014d9cdd13a
Louis <louisapstg@gmail.com>
2022-12-09 15:10:30 +0700

    feat/apis: added instructor api

---
+++ src/apis/instructor.api.js
@@ -0,0 +1,67 @@
+import axiosInstance from "../configs/axiosInstance";
+
+const InstructorAPI = {
+    async getInstructor(limit, page) {
+        try {
+            const config = {
+                params: {
+                    limit,
+                    page,
+                },
+            };
+            const response = await axiosInstance.get("/instructors", config);
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+    async searchInstructor(keyword) {
+        try {
+            const response = await axiosInstance.get(`/instructors?query=${keyword}`);
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+    async createInstructor(data) {
+        try {
+            const response = await axiosInstance.post("/instructors", data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+    async editInstructor(data) {
+        try {
+            const instructor_id = data.instructor_id;
+            const response = await axiosInstance.patch(`/instructors/${instructor_id}`, data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+    async deleteInstructor(instructor_id) {
+        try {
+            const response = await axiosInstance.delete(`/instructors/${instructor_id}`);
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+};
+
+export default InstructorAPI;
