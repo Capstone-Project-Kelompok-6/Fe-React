3763440d4a5a33aed3bc8fa55cbc3f80db03678e
Louis <louisapstg@gmail.com>
2022-12-09 15:10:54 +0700

    feat/apis: added workout api

---
+++ src/apis/workout.api.js
@@ -0,0 +1,66 @@
+import axiosInstance from "../configs/axiosInstance";
+
+const WorkoutAPI = {
+    async getWorkout(limit, page) {
+        try {
+            const config = {
+                params: {
+                    limit,
+                    page,
+                },
+            };
+            const response = await axiosInstance.get("/workouts", config);
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+    async searchWorkout(keyword) {
+        try {
+            const response = await axiosInstance.get(`/workouts?query=${keyword}`);
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+    async createWorkout(data) {
+        try {
+            const response = await axiosInstance.post("/workouts", data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+    async editWorkout(data) {
+        try {
+            const workout_id = data.workout_id;
+            const response = await axiosInstance.patch(`/workouts/${workout_id}`, data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+    async deleteWorkout(workout_id) {
+        try {
+            const response = await axiosInstance.delete(`/workouts/${workout_id}`);
+            return response;
+        } catch (error) {
+            const { message } = error.response.data;
+            throw new Error(message);
+        }
+    },
+};
+
+export default WorkoutAPI;
