155cc69e5255ab69ab83a54c2c3cca9188e436ce
Louis <louisapstg@gmail.com>
2022-12-11 00:01:44 +0700

    feat/offline-booking: add layout and header for offline booking

---
+++ src/components/Admin/Booking/LayoutBooking.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const LayoutBooking = () => {
+	return (
+		<>
+			<Outlet />
+		</>
+	);
+};
+
+export default LayoutBooking;
