eb06336e19132c70d1345fa08b1a4729a5c210f8
Louis <louisapstg@gmail.com>
2022-12-10 18:05:20 +0700

    feat/workout: add workout pages and routing

---
+++ src/pages/Admin/WorkoutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WorkoutList from "../../components/Admin/Workout/WorkoutList";
+
+const WorkoutPage = () => {
+	return (
+		<>
+			<WorkoutList />
+		</>
+	);
+};
+
+export default WorkoutPage;
