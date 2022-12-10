71590bb9cd52303e1ecdc84772d4d7f69f9a064e
Louis <louisapstg@gmail.com>
2022-12-10 18:07:22 +0700

    feat/workout: added logic funtion handle submit and update

--- src/components/Admin/Workout/ModalCreateWorkout.jsx
+++ src/components/Admin/Workout/ModalCreateWorkout.jsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState, useRef } from "react";
-import { cancelButton, inputError, inputNotError, labelError, labelNotError, regexNameValidation, saveButton } from "../../../utils/globalVariable";
+import { useDispatch } from "react-redux";
+import Swal from "sweetalert2";
+import { createWorkout } from "../../../stores/features/workoutSlice";
+import {
+	cancelButton,
+	inputError,
+	inputNotError,
+	labelError,
+	labelNotError,
+	regexNameValidation,
+	saveButton,
+} from "../../../utils/globalVariable";
 
 const baseValues = {
 	workout: "",
@@ -17,6 +28,7 @@ const ModalCreateWorkout = ({ handleModalCreateTrigger }) => {
 	const [errors, setErrors] = useState(baseErrors);
 	const [values, setValues] = useState(baseValues);
 	const imageWorkout = useRef(null);
+	const dispatch = useDispatch();
 
 	const MAX_FILE_SIZE = 1024;
 
@@ -32,9 +44,15 @@ const ModalCreateWorkout = ({ handleModalCreateTrigger }) => {
 	const validation = (name, value) => {
 		if (name === "workout") {
 			if (!regexNameValidation.test(value)) {
-				setErrors({ ...errors, workout: "Workout name must be in letters" });
+				setErrors({
+					...errors,
+					workout: "Workout name must be in letters",
+				});
 			} else if (value == "") {
-				setErrors({ ...errors, workout: "Workout name is required" });
+				setErrors({
+					...errors,
+					workout: "Workout name is required",
+				});
 			} else {
 				setErrors({ ...errors, workout: "" });
 			}
@@ -50,7 +68,10 @@ const ModalCreateWorkout = ({ handleModalCreateTrigger }) => {
 		const fileSizeKiloBytes = file.size / 1024;
 
 		if (fileSizeKiloBytes > MAX_FILE_SIZE) {
-			setErrors({ ...errors, image: "File size is greater than maximum limit" });
+			setErrors({
+				...errors,
+				image: "File size is greater than maximum limit",
+			});
 			return;
 		} else {
 			setErrors({ ...errors, image: "" });
@@ -80,6 +101,57 @@ const ModalCreateWorkout = ({ handleModalCreateTrigger }) => {
 		};
 	}, [file]);
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const workout = formData.get("workout");
+		const image = formData.get("image");
+		const description = formData.get("description");
+
+		if (!errors.workout && !errors.image) {
+			try {
+				dispatch(
+					createWorkout({ workout, image, description }),
+				).then((res) => {
+					if (!res.error) {
+						setTimeout(
+							() =>
+								Swal.fire({
+									icon: "success",
+									title: "Saved",
+									text: "Workout data successfully saved",
+									showConfirmButton: false,
+									timer: 2000,
+									background: "#ffffff",
+								}),
+							1000,
+						);
+						handleModalCreateTrigger();
+					} else {
+						Swal.fire(
+							"Sorry",
+							res.error.message.split(":")[1],
+							"info",
+						);
+					}
+				});
+			} catch (error) {
+				Swal.fire("Sorry", error.message.split(":")[1], "info");
+			}
+		} else {
+			setTimeout(
+				() =>
+					Swal.fire({
+						icon: "error",
+						title: "Workout data cannot saved",
+						text: "Please, check your inputed data",
+						background: "#ffffff",
+					}),
+				1000,
+			);
+		}
+	};
+
 	const handleCancelUpload = () => {
 		setFileDataURL("");
 		imageWorkout.current.value = "";
@@ -87,14 +159,19 @@ const ModalCreateWorkout = ({ handleModalCreateTrigger }) => {
 
 	return (
 		<div className="relative z-50">
-			<div className="fixed inset-0 z-50 bg-gray-400 bg-opacity-50 transition-opacity"></div>
+			<div className="fixed inset-0 z-50 bg-gray-600 bg-opacity-80 transition-opacity"></div>
 
 			<div className="fixed inset-0 z-50 items-center justify-center overflow-y-auto">
 				<div className="flex w-full items-end justify-center px-4 py-16 sm:h-full sm:items-center sm:p-0 md:h-full">
 					<div className="relative h-full w-full max-w-sm sm:max-w-sm md:h-auto md:max-w-md lg:max-w-lg xl:max-w-xl">
-						<form className="rounded-xl bg-white shadow">
-							<div className="flex items-center justify-between rounded-t border-b p-4">
-								<h3 className="p-1.5 text-base font-bold text-neutral-100-2 lg:text-lg xl:text-xl">Add New Workout</h3>
+						<form
+							onSubmit={handleSubmit}
+							className="rounded-xl bg-white shadow"
+						>
+							<div className="flex items-center justify-between rounded-t p-4">
+								<h3 className="p-1.5 text-base font-bold text-neutral-100-2 lg:text-lg xl:text-xl">
+									Add New Workout
+								</h3>
 							</div>
 							<div className="h-[65vh] overflow-y-auto p-6">
 								<div className="h-[90&] space-y-6">
@@ -104,19 +181,37 @@ const ModalCreateWorkout = ({ handleModalCreateTrigger }) => {
 												type="text"
 												id="workout"
 												name="workout"
-												className={errors.workout ? inputError : inputNotError}
+												className={
+													errors.workout
+														? inputError
+														: inputNotError
+												}
 												placeholder=" "
-												onChange={handleChange}
+												onChange={
+													handleChange
+												}
 												required
 											/>
-											<label htmlFor="workout" className={errors.workout ? labelError : labelNotError}>
-												<span className="block after:ml-1 after:text-red-500 after:content-['*']">Workout Name</span>
+											<label
+												htmlFor="workout"
+												className={
+													errors.workout
+														? labelError
+														: labelNotError
+												}
+											>
+												<span className="block after:ml-1 after:text-red-500 after:content-['*']">
+													Workout Name
+												</span>
 											</label>
 										</div>
 										<div className="mt-1">
 											{errors.workout && (
 												<span className="text-xs font-light text-secondary-red md:text-sm">
-													<i className="fi fi-rr-info"></i> {errors.workout}
+													<i className="fi fi-rr-info"></i>{" "}
+													{
+														errors.workout
+													}
 												</span>
 											)}
 										</div>
@@ -126,14 +221,19 @@ const ModalCreateWorkout = ({ handleModalCreateTrigger }) => {
 											<div className="my-5 flex w-full items-center justify-center">
 												<div className="flex flex-col items-center justify-center">
 													<img
-														src={fileDataURL}
+														src={
+															fileDataURL
+														}
 														alt=""
 														className="h-52 w-80 rounded-lg border-2 border-dashed border-neutral-80 object-cover object-center"
 													/>
 													<button
 														type="button"
 														className="absolute -top-2 -right-2 inline-flex h-8 w-8 items-center justify-center rounded-full border-2 border-white bg-red-500 text-sm font-bold text-white sm:mr-10 md:right-0 md:mr-14 xl:mr-20"
-														onClick={handleCancelUpload}>
+														onClick={
+															handleCancelUpload
+														}
+													>
 														<i className="fi fi-rr-cross-small mt-1"></i>
 													</button>
 												</div>
@@ -154,33 +254,58 @@ const ModalCreateWorkout = ({ handleModalCreateTrigger }) => {
 											type="file"
 											accept="image/*"
 											ref={imageWorkout}
-											onChange={handleUploadImage}
+											onChange={
+												handleUploadImage
+											}
 											required
 										/>
 										<div className="mb-4 flex items-center space-x-4">
 											{errors.image && (
 												<span className="text-xs font-light text-secondary-red md:text-sm">
-													<i className="fi fi-rr-info"></i> {errors.image}
+													<i className="fi fi-rr-info"></i>{" "}
+													{errors.image}
 												</span>
 											)}
 											<div className="min-w-0 flex-1">
-												<p className="text-end text-xs font-medium text-neutral-100-2 md:text-sm">Max size: 1MB</p>
+												<p className="text-end text-xs font-medium text-neutral-100-2 md:text-sm">
+													Max size: 1MB
+												</p>
 											</div>
 										</div>
 									</div>
 									<div className="relative">
-										<textarea id="description" name="description" type="text" rows="5" className={inputNotError} placeholder=" " required></textarea>
-										<label htmlFor="description" className={labelNotError}>
-											<span className="block after:ml-1 after:text-red-500 after:content-['*']"> Description</span>
+										<textarea
+											id="description"
+											name="description"
+											type="text"
+											rows="5"
+											className={inputNotError}
+											placeholder=" "
+											required
+										></textarea>
+										<label
+											htmlFor="description"
+											className={labelNotError}
+										>
+											<span className="block after:ml-1 after:text-red-500 after:content-['*']">
+												Information
+											</span>
 										</label>
 									</div>
 								</div>
 							</div>
-							<div className="flex items-center justify-center space-x-2 border-t border-gray-200 p-6">
-								<button type="button" className={cancelButton} onClick={handleModalCreateTrigger}>
+							<div className="flex items-center justify-center space-x-2 p-6">
+								<button
+									type="button"
+									className={cancelButton}
+									onClick={handleModalCreateTrigger}
+								>
 									Cancel
 								</button>
-								<button type="submit" className={saveButton}>
+								<button
+									type="submit"
+									className={saveButton}
+								>
 									Save
 								</button>
 							</div>
