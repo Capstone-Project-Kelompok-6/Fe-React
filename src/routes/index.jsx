c2159341d0eaf058c44113f66e96875b69fc0841
Louis <louisapstg@gmail.com>
2022-12-11 01:04:51 +0700

    feat/online-booking: added online booking page and route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -14,6 +14,7 @@ import {
 	OnlineClassesPage,
 	WorkoutPage,
 	OfflineBookingPage,
+	OnlineBookingPage,
 } from "../pages";
 import PrivateRoute from "./PrivateRoute";
 import ProtectedRoute from "./ProtectedRoute";
@@ -65,6 +66,10 @@ const Router = () => {
 								<Route
 									path="offline"
 									element={<OfflineBookingPage />}
+								/>
+								<Route
+									path="online"
+									element={<OnlineBookingPage />}
 								/>
 							</Route>
 						</Route>
