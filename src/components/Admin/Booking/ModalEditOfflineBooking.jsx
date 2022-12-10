af0f2aa5c43a0807efc75042b08a322829d7b726
Louis <louisapstg@gmail.com>
2022-12-11 00:03:10 +0700

    feat/offline-booking: added logic function for modal edit and create

--- src/components/Admin/Booking/ModalEditOfflineBooking.jsx
+++ src/components/Admin/Booking/ModalEditOfflineBooking.jsx
@@ -1,8 +1,57 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import Select from "react-select";
-import { cancelButton, saveButton } from "../../../utils/globalVariable";
+import Swal from "sweetalert2";
+import { editOfflineBooking } from "../../../stores/features/offlineBookingSlice";
+import {
+	cancelButton,
+	saveButton,
+	select,
+} from "../../../utils/globalVariable";
 
-const ModalEditOfflineBooking = ({ handleModalEditTrigger, handleActionDropdown }) => {
+const ModalEditOfflineBooking = ({
+	handleModalEditTrigger,
+	handleActionDropdown,
+	offlineClassesList,
+	update,
+}) => {
+	const { book_id, class_id, workout, instructor_name } = update;
+	const dispatch = useDispatch();
+
+	const handleUpdate = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const class_id = formData.get("class_id");
+		const result = dispatch(editOfflineBooking({ book_id, class_id }));
+		if (result) {
+			handleModalEditTrigger();
+			handleActionDropdown();
+			setTimeout(
+				() =>
+					Swal.fire({
+						icon: "success",
+						title: "Saved",
+						text: "Offline booking data successfully saved",
+						background: "#ffffff",
+					}),
+				1000,
+			);
+		} else {
+			setTimeout(
+				() =>
+					Swal.fire({
+						icon: "success",
+						title: "Saved",
+						text: "Offline booking data successfully saved",
+						showConfirmButton: false,
+						timer: 2000,
+						background: "#ffffff",
+					}),
+				1000,
+			);
+		}
+	};
+
 	return (
 		<div className="relative z-50">
 			<div className="fixed inset-0 z-50 bg-gray-400 bg-opacity-50 transition-opacity"></div>
@@ -10,26 +59,56 @@ const ModalEditOfflineBooking = ({ handleModalEditTrigger, handleActionDropdown 
 			<div className="fixed inset-0 z-50 items-center justify-center overflow-y-auto">
 				<div className="flex w-full items-end justify-center px-4 pt-16 pb-6 sm:h-full sm:items-center sm:p-0 md:h-full">
 					<div className="relative h-full w-full max-w-sm sm:max-w-sm md:h-auto md:max-w-md lg:max-w-lg xl:max-w-xl">
-						<form className="rounded-xl bg-white shadow">
+						<form
+							onSubmit={handleUpdate}
+							className="rounded-xl bg-white shadow"
+						>
 							<div className="flex items-center justify-between rounded-t border-b p-4">
-								<h3 className="p-1.5 text-base font-bold text-neutral-100-2 lg:text-lg xl:text-xl">Edit Offline Booking</h3>
+								<h3 className="p-1.5 text-base font-bold text-neutral-100-2 lg:text-lg xl:text-xl">
+									Edit Offline Booking
+								</h3>
 							</div>
 							<div className="space-y-6 p-6">
 								<div className="relative">
 									<Select
-										className="z-30 block w-full appearance-none rounded-lg bg-transparent text-sm font-normal text-neutral-60 focus:border-blue-600 focus:outline-none focus:ring-0"
+										className={`z-30 ${select}`}
+										options={offlineClassesList.rows
+											?.map((item) => {
+												return {
+													value: item.class_id,
+													label: `${item.workout} - ${item.instructor_name}`,
+												};
+											})
+											.sort((a, b) =>
+												a.label.localeCompare(
+													b.label,
+												),
+											)}
 										name="class_id"
+										defaultValue={{
+											value: class_id,
+											label: `${workout} - ${instructor_name}`,
+										}}
 										placeholder="Select offline classes"
-										noOptionsMessage={() => "Membership data not found"}
+										noOptionsMessage={() =>
+											"Membership data not found"
+										}
 										isClearable
 									/>
 								</div>
 							</div>
 							<div className="flex items-center justify-center space-x-2 border-t border-gray-200 p-6">
-								<button type="button" className={cancelButton} onClick={handleModalEditTrigger}>
+								<button
+									type="button"
+									className={cancelButton}
+									onClick={handleModalEditTrigger}
+								>
 									Cancel
 								</button>
-								<button type="submit" className={saveButton}>
+								<button
+									type="submit"
+									className={saveButton}
+								>
 									Save
 								</button>
 							</div>
