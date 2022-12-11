8107e74d0f08297113a86ea2ef1630a71ccb35aa
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-12-11 09:16:01 +0700

    feat/article: added to top components for articke and booking offline online page

--- src/pages/Admin/ArticlePage.jsx
+++ src/pages/Admin/ArticlePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ArticleList from "../../components/Admin/Article/ArticleList";
+import ToTop from "../../components/ToTop";
 
 const ArticlePage = () => {
 	return (
 		<>
 			<ArticleList />
+			<ToTop />
 		</>
 	);
 };
