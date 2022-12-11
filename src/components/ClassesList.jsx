51fb05603adec9a4e1e17a4acca3adf375f585b3
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-12-11 09:14:32 +0700

    feat/article: update classes list and instructor info components in landing page now can display data from API

--- src/components/ClassesList.jsx
+++ src/components/ClassesList.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-import yogaClass from "../assets/img/png/yogaClass.png";
-import fitness from "../assets/img/png/fitness.png";
-import muscle from "../assets/img/png/muscle.png";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchOfflineClasses } from "../stores/features/offlineClassesSlice";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -11,12 +9,20 @@ import SwiperCore, { Pagination, Autoplay } from "swiper";
 
 const ClassesList = () => {
 	SwiperCore.use([Autoplay]);
+	const dispatch = useDispatch();
+	const offlineClasses = useSelector((state) => state.offlineClasses.data);
+
+	useEffect(() => {
+		dispatch(fetchOfflineClasses(5));
+	}, [dispatch]);
+
+	const truncate = (string, n) => {
+		return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+	};
 
 	return (
 		<div className="bg-secondary-navy pt-24 pb-16 lg:px-20">
-			<h1 className="text-center text-xl font-medium text-white lg:text-4xl">
-				Lets Join Our Classes
-			</h1>
+			<h1 className="text-center text-xl font-medium text-white lg:text-4xl">Lets Join Our Classes</h1>
 			<Swiper
 				slidesPerView={3}
 				spaceBetween={30}
@@ -40,118 +46,28 @@ const ClassesList = () => {
 					delay: 1500,
 					pauseOnMouseEnter: true,
 					disableOnInteraction: false,
-				}}
-			>
-				<SwiperSlide className="bg-secondary-navy">
-					<div className="mx-auto mt-8 max-w-xs rounded-lg bg-[#252525] bg-opacity-40 shadow-md lg:mt-14">
-						<Link href="#">
-							<img
-								className="mx-auto rounded-t-lg lg:p-4 lg:pb-0"
-								src={yogaClass}
-								alt=""
-							/>
-						</Link>
-						<div className="p-5">
-							<Link href="#">
-								<h5 className="mb-2 text-lg font-medium tracking-tight text-white lg:text-3xl lg:font-semibold">
-									Yoga
-								</h5>
-							</Link>
-							<p className="mb-3 text-sm font-light text-white lg:text-lg lg:font-normal">
-								Here are the biggest enterprise technology acquisitions
-								of 2021 so far, in reverse chronological order.
-							</p>
-						</div>
-					</div>
-				</SwiperSlide>
-				<SwiperSlide className="bg-secondary-navy">
-					<div className="mx-auto mt-8 max-w-xs rounded-lg bg-[#252525] bg-opacity-40 shadow-md lg:mt-14">
-						<Link href="#">
-							<img
-								className="mx-auto rounded-t-lg lg:p-4 lg:pb-0"
-								src={fitness}
-								alt=""
-							/>
-						</Link>
-						<div className="p-5">
-							<Link href="#">
-								<h5 className="mb-2 text-lg font-medium tracking-tight text-white lg:text-3xl lg:font-semibold">
-									Yoga
-								</h5>
-							</Link>
-							<p className="mb-3 text-sm font-light text-white lg:text-lg lg:font-normal">
-								Here are the biggest enterprise technology acquisitions
-								of 2021 so far, in reverse chronological order.
-							</p>
-						</div>
-					</div>
-				</SwiperSlide>
-				<SwiperSlide className="bg-secondary-navy">
-					<div className="mx-auto mt-8 max-w-xs rounded-lg bg-[#252525] bg-opacity-40 shadow-md lg:mt-14">
-						<Link href="#">
-							<img
-								className="mx-auto rounded-t-lg lg:p-4 lg:pb-0"
-								src={muscle}
-								alt=""
-							/>
-						</Link>
-						<div className="p-5">
-							<Link href="#">
-								<h5 className="mb-2 text-lg font-medium tracking-tight text-white lg:text-3xl lg:font-semibold">
-									Yoga
-								</h5>
-							</Link>
-							<p className="mb-3 text-sm font-light text-white lg:text-lg lg:font-normal">
-								Here are the biggest enterprise technology acquisitions
-								of 2021 so far, in reverse chronological order.
-							</p>
-						</div>
-					</div>
-				</SwiperSlide>
-				<SwiperSlide className="bg-secondary-navy">
-					<div className="mx-auto mt-8 max-w-xs rounded-lg bg-[#252525] bg-opacity-40 shadow-md lg:mt-14">
-						<Link href="#">
-							<img
-								className="mx-auto rounded-t-lg lg:p-4 lg:pb-0"
-								src={yogaClass}
-								alt=""
-							/>
-						</Link>
-						<div className="p-5">
-							<Link href="#">
-								<h5 className="mb-2 text-lg font-medium tracking-tight text-white lg:text-3xl lg:font-semibold">
-									Yoga
-								</h5>
-							</Link>
-							<p className="mb-3 text-sm font-light text-white lg:text-lg lg:font-normal">
-								Here are the biggest enterprise technology acquisitions
-								of 2021 so far, in reverse chronological order.
-							</p>
-						</div>
-					</div>
-				</SwiperSlide>
-				<SwiperSlide className="bg-secondary-navy">
-					<div className="mx-auto mt-8 max-w-xs rounded-lg bg-[#252525] bg-opacity-40 shadow-md lg:mt-14">
-						<Link href="#">
-							<img
-								className="mx-auto rounded-t-lg lg:p-4 lg:pb-0"
-								src={yogaClass}
-								alt=""
-							/>
-						</Link>
-						<div className="p-5">
-							<Link href="#">
-								<h5 className="mb-2 text-lg font-medium tracking-tight text-white lg:text-3xl lg:font-semibold">
-									Yoga
-								</h5>
-							</Link>
-							<p className="mb-3 text-sm font-light text-white lg:text-lg lg:font-normal">
-								Here are the biggest enterprise technology acquisitions
-								of 2021 so far, in reverse chronological order.
-							</p>
-						</div>
-					</div>
-				</SwiperSlide>
+				}}>
+				{offlineClasses.rows?.map((item) => {
+					return (
+						<SwiperSlide key={item.class_id}>
+							<div className="mx-auto mt-8 h-full max-w-xs rounded-lg bg-[#252525] bg-opacity-40 shadow-md lg:mt-14">
+								<div>
+									<img
+										className="inset-0 mx-auto h-44 w-full rounded-t-lg object-cover object-center lg:p-4 lg:pb-0"
+										src={item.workout_image}
+										alt={item.image_name}
+									/>
+								</div>
+								<div className="p-5">
+									<div>
+										<h5 className="mb-2 text-lg font-medium tracking-tight text-white lg:text-3xl lg:font-semibold">{item.workout}</h5>
+									</div>
+									<p className="mb-3 text-sm font-light text-white lg:text-lg lg:font-normal">{truncate(item.description, 50)}</p>
+								</div>
+							</div>
+						</SwiperSlide>
+					);
+				})}
 			</Swiper>
 		</div>
 	);
