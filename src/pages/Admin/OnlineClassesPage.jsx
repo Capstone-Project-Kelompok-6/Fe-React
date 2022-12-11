ba06edda1cd7a95764222c32ce248e6b93581e78
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-12-11 19:11:38 +0700

    feat/article: added configuration react helmet on online booking, online classes, and workout pages

--- src/pages/Admin/OnlineClassesPage.jsx
+++ src/pages/Admin/OnlineClassesPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import OnlineClassesList from "../../components/Admin/Classes/OnlineClassesList";
 import ToTop from "../../components/ToTop";
 
 const OnlineClassesPage = () => {
 	return (
 		<>
+			<Helmet>
+				<title>Manage Online Classes - Gym Membership</title>
+				<meta name="website" content="Gym Membership" />
+			</Helmet>
 			<OnlineClassesList />
 			<ToTop />
 		</>
