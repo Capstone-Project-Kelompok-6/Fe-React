812f5f5ed727f0f736652aa418e098d2077f0537
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-12-16 00:05:31 +0700

    update-v0.6: update custom message required on form login component and react player css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,26 +6,32 @@ const Footer = () => {
 			<footer className="bg-secondary-navy py-5 px-6 dark:bg-gray-900 sm:py-16 lg:py-10 lg:px-36">
 				<div className="flex-none text-white lg:flex lg:justify-between lg:text-left">
 					<div className="mb-6 lg:mb-0">
-						<h3 className="text-2xl font-semibold lg:text-5xl">
-							WorkFit.
-						</h3>
+						<h3 className="text-2xl font-semibold lg:text-5xl">WorkFit.</h3>
 						<div className="text-base font-medium text-secondary-subtle-yellow lg:text-xl">
 							<p className="mt-2 lg:my-3">workfit@gmail.com</p>
 							<p>+62 821 10001 778</p>
 						</div>
 						<div className="mt-4 flex lg:mt-5">
-							<div className="border-r pr-6 pt-1">
+							<a
+								className="border-r pr-6 pt-1"
+								href="https://www.youtube.com/@AlterraAcademy"
+								target="_blank"
+								rel="noopener noreferrer">
 								<i className="fi fi-brands-facebook text-xl lg:text-3xl"></i>
-							</div>
+							</a>
 							<div className="border-r px-6 pt-1">
 								<i className="fi fi-brands-instagram text-xl lg:text-3xl"></i>
 							</div>
 							<div className="border-r px-6 pt-1">
 								<i className="fi fi-brands-twitter text-xl lg:text-3xl"></i>
 							</div>
-							<div className="px-6 pt-1">
+							<a
+								className="px-6 pt-1"
+								href="https://www.youtube.com/@AlterraAcademy"
+								target="_blank"
+								rel="noopener noreferrer">
 								<i className="fi fi-brands-youtube text-xl lg:text-3xl"></i>
-							</div>
+							</a>
 						</div>
 					</div>
 					<div className="mb-6 md:mb-0">
@@ -34,42 +40,27 @@ const Footer = () => {
 						</h2>
 						<ul className="text-base text-secondary-subtle-yellow dark:text-gray-400 lg:text-xl">
 							<li className="mb-1">
-								<a
-									href="https://flowbite.com/"
-									className="hover:underline"
-								>
+								<a href="https://flowbite.com/" className="hover:underline">
 									About
 								</a>
 							</li>
 							<li className="mb-1">
-								<a
-									href="https://tailwindcss.com/"
-									className="hover:underline"
-								>
+								<a href="https://tailwindcss.com/" className="hover:underline">
 									Features
 								</a>
 							</li>
 							<li className="mb-1">
-								<a
-									href="https://tailwindcss.com/"
-									className="hover:underline"
-								>
+								<a href="https://tailwindcss.com/" className="hover:underline">
 									Blog
 								</a>
 							</li>
 							<li className="mb-1">
-								<a
-									href="https://tailwindcss.com/"
-									className="hover:underline"
-								>
+								<a href="https://tailwindcss.com/" className="hover:underline">
 									Contact Us
 								</a>
 							</li>
 							<li className="mb-1">
-								<a
-									href="https://tailwindcss.com/"
-									className="hover:underline"
-								>
+								<a href="https://tailwindcss.com/" className="hover:underline">
 									FAQ
 								</a>
 							</li>
@@ -82,16 +73,14 @@ const Footer = () => {
 						<ul className="text-base text-secondary-subtle-yellow dark:text-gray-400 lg:text-xl">
 							<li className="mb-4">
 								<p>
-									Subscribe our newsletter to get our latest <br />{" "}
-									update & news
+									Subscribe our newsletter to get our latest <br /> update & news
 								</p>
 							</li>
 							<li>
 								<form>
 									<label
 										htmlFor="search"
-										className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
-									>
+										className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">
 										Search
 									</label>
 									<div className="relative">
@@ -101,8 +90,7 @@ const Footer = () => {
 												className="h-5 w-5 text-gray-500 dark:text-gray-400"
 												fill="currentColor"
 												viewBox="0 0 20 20"
-												xmlns="http://www.w3.org/2000/svg"
-											>
+												xmlns="http://www.w3.org/2000/svg">
 												<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
 												<path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
 											</svg>
