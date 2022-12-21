03e9e9c8a212bf9980fc976842bc33117f6580dc
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-12-21 13:43:16 +0700

    release-v1.0.1: delete redux devtools on production mode

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,6 +3,7 @@ import { rootReducer } from "./features";
 
 const store = configureStore({
 	reducer: rootReducer,
+	devTools: import.meta.env.NODE_ENV !== "production",
 });
 
 export default store;
