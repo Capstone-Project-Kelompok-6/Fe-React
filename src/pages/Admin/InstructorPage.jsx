3a42a906cce51e72a2498fc20b00563f23cf476b
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-12-21 13:43:49 +0700

    release-v1.0.1: delete notification booking on admin pages and change into main layout component

--- src/pages/Admin/InstructorPage.jsx
+++ src/pages/Admin/InstructorPage.jsx
@@ -1,43 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import InstructorList from "../../components/Admin/Instructor/InstructorList";
-import audio from "../../assets/audio/notification.mp3";
-import Pusher from "pusher-js";
-import CONST from "../../utils/constants";
-import Swal from "sweetalert2";
 
 const InstructorPage = () => {
-	const playAudio = () => {
-		const audioToPlay = new Audio(audio);
-		audioToPlay.play();
-	};
-
-	const pusher = new Pusher(CONST.PUSHER_KEY, {
-		cluster: "ap1",
-	});
-
-	const channel = pusher.subscribe("my-channel");
-
-	channel.bind("notification", (data) => {
-		const Toast = Swal.mixin({
-			toast: true,
-			position: "top-end",
-			showConfirmButton: false,
-			timer: 3000,
-			timerProgressBar: true,
-			didOpen: (toast) => {
-				toast.addEventListener("mouseenter", Swal.stopTimer);
-				toast.addEventListener("mouseleave", Swal.resumeTimer);
-			},
-		});
-
-		Toast.fire({
-			icon: "success",
-			title: data,
-		});
-		playAudio();
-	});
-
 	return (
 		<>
 			<Helmet>
--- src/pages/Admin/OfflineBookingPage.jsx
+++ src/pages/Admin/OfflineBookingPage.jsx
@@ -2,43 +2,8 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import OfflineBookingList from "../../components/Admin/Booking/OfflineBookingList";
 import ToTop from "../../components/ToTop";
-import audio from "../../assets/audio/notification.mp3";
-import Pusher from "pusher-js";
-import CONST from "../../utils/constants";
-import Swal from "sweetalert2";
 
 const OfflineBookingPage = () => {
-	const playAudio = () => {
-		const audioToPlay = new Audio(audio);
-		audioToPlay.play();
-	};
-
-	const pusher = new Pusher(CONST.PUSHER_KEY, {
-		cluster: "ap1",
-	});
-
-	const channel = pusher.subscribe("my-channel");
-
-	channel.bind("notification", (data) => {
-		const Toast = Swal.mixin({
-			toast: true,
-			position: "top-end",
-			showConfirmButton: false,
-			timer: 3000,
-			timerProgressBar: true,
-			didOpen: (toast) => {
-				toast.addEventListener("mouseenter", Swal.stopTimer);
-				toast.addEventListener("mouseleave", Swal.resumeTimer);
-			},
-		});
-
-		Toast.fire({
-			icon: "success",
-			title: data,
-		});
-		playAudio();
-	});
-
 	return (
 		<>
 			<Helmet>
